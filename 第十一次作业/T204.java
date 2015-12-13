11acad0e1d508e853278097c4a728442bebafc19
devilguo <512575903@qq.com>
2015-12-13 13:52:52 +0800

    11

---
+++ 第十一次作业/T204.java
@@ -0,0 +1,19 @@
+public class Solution {
+    //删除掉所有不是素数的
+    public int countPrimes(int n) {
+        boolean[] a = new boolean[n];
+     for(int i=2; i*i<n; i++) {
+        if(!a[i]) {
+            for(int j=i; i*j<n; j++) {
+                a[i*j] = true;
+            }
+        }
+     }
+     int c=0;
+     
+     for(int i=2; i<n; i++) {
+         if(a[i] == false) ++c;
+     }
+     return c;
+    }
+}
